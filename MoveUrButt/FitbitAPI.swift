2c9fd1580e49112cd5afca73abb2cc56af35cdbc
caroline6927 <caroline6927@gmail.com>
2016-09-09 12:12:01 +0800

    Setting up OAuth2 library
     - OAuth2 library added
     - OAuth2 functions can enabled with “import OAuth2”

---
+++ MoveUrButt/FitbitAPI.swift
@@ -0,0 +1,14 @@
+//
+//  FitbitAPI.swift
+//  MoveUrButt
+//
+//  Created by Wang Yujia on 8/9/16.
+//  Copyright © 2016 National University of Singapore Design Centric Program. All rights reserved.
+//
+
+import Foundation
+import OAuth2
+
+
+
+
